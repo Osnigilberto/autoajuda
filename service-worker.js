0f9d74b81dc6e992c5419d234338a27461a1b338
Osnigilberto <uuujuninho@gmail.com>
2025-04-17 23:23:03 -0300

    create a PWA

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,48 @@
+const CACHE_NAME = 'respira-cache-v1';
+
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('respira-cache').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/style.css',
-          '/script.js',
-          '/assets/icons/icon-192x192.png',
-          '/assets/icons/icon-512x512.png'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        return cachedResponse || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/assets/icons/icon-192x192.png',
+        '/assets/icons/icon-512x512.png',
+        '/assets/icons/favicon-16x16.png',
+        '/assets/icons/favicon-32x32.png',
+        '/assets/icons/favicon.ico',
+        '/assets/icons/apple-touch-icon.png',
+        '/assets/sounds/bell-a.mp3',
+        '/assets/sounds/chuva.mp3',
+        '/assets/sounds/floresta.mp3',
+        '/assets/sounds/mar.mp3',
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
